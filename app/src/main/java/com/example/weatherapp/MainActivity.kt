62844d539f6a1aabdb773fe5a4321a14215a5f7d
Frranky <srfranky228@gmail.com>
2022-12-07 23:48:18 +0700

    006-Added a graph

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,61 +1,57 @@
 package com.example.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapp.data.api.getWeather
 import com.example.weatherapp.data.mapper.toForecastModel
 import com.example.weatherapp.data.model.ForecastModel
 import com.example.weatherapp.databinding.ActivityMainBinding
-import com.google.gson.GsonBuilder
-import com.google.gson.internal.bind.util.ISO8601Utils.format
+import com.example.weatherapp.domain.usecase.getData
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var binding: ActivityMainBinding
+	private lateinit var data: ArrayList<ForecastModel>
+
+	private var formatter: ValueFormatter = object : ValueFormatter() {
+		override fun getAxisLabel(value: Float, axis: AxisBase): String {
+			return SimpleDateFormat("MM.dd HH:mm").format(SimpleDateFormat("yyyy-MM-dd HH:mm").parse(data[value.toInt()].date))
+		}
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
-		val currentDate = SimpleDateFormat("yyyy-MM-dd HH:mm").parse(SimpleDateFormat("yyyy-MM-dd HH:mm").format(Date()))
+		val currentDate = SimpleDateFormat("yyyy-MM-dd HH:mm").parse(SimpleDateFormat("yyyy-MM-dd HH:mm").format(Date())).time
+		val context = this.baseContext
 
 		GlobalScope.launch {
-			val data = toForecastModel(getWeather())
-			saveData(data)
+			data = getData(currentDate, context)
+			val entries = arrayListOf<Entry>()
 
-			val cash = readData()//toForecastModel(readData())
-			val currentDate = cash.first
-			val data1 = toForecastModel(cash.second)
+			for(i in 0 until data.size) {
+				entries.add(Entry(i.toFloat(), data[i].temp.toFloat()))
+			}
+			val dataset = LineDataSet(entries, "Temperature")
+			dataset.axisDependency = YAxis.AxisDependency.LEFT
 
-			runOnUiThread {
-				binding.text.text = /*"$cashDate ${currentDate.time}"*/"${data[0].temp} ${data[0].feels_like} && ${data1[0].temp} ${data1[0].feels_like}"
+			launch(Dispatchers.Main) {
+				binding.chart.data = LineData(dataset)
+				binding.chart.xAxis.valueFormatter = formatter
+				binding.chart.invalidate()
 			}
 		}
 		setContentView(binding.root)
-	}
-
-	private fun saveData(data: ArrayList<ForecastModel>) {
-		val gsonBuilder = GsonBuilder()
-		val gson = gsonBuilder.create()
-		val json = gson.toJson(data)
-		val path = this.baseContext.filesDir
-		val letDirectory = File(path, "json")
-		letDirectory.mkdirs()
-		val file = File(letDirectory, "data.json")
-		file.appendText(json)
-	}
-
-	private fun readData(): Pair<Long, String> {
-		val path = this.baseContext.filesDir
-		val letDirectory = File(path, "json")
-		letDirectory.mkdirs()
-		val file = File(letDirectory, "data.json")
-		return file.lastModified() to FileInputStream(file).bufferedReader().use { it.readText() }
 	}
 }
---
+++ app/src/main/java/com/example/weatherapp/domain/usecase/getDataUseCase.kt
@@ -0,0 +1,34 @@
+package com.example.weatherapp.domain.usecase
+
+import android.content.Context
+import com.example.weatherapp.data.api.getWeather
+import com.example.weatherapp.data.mapper.toForecastModel
+import com.example.weatherapp.data.model.ForecastModel
+import com.google.gson.GsonBuilder
+import java.io.File
+import java.io.FileInputStream
+
+fun getData(currentDate: Long, context: Context): ArrayList<ForecastModel> {
+	val path = context.filesDir
+	val letDirectory = File(path, "json")
+	letDirectory.mkdirs()
+	val file = File(letDirectory, "data.json")
+
+	if(currentDate - file.lastModified() < 600)
+		return toForecastModel(FileInputStream(file).bufferedReader().use { it.readText() })
+
+	val data = toForecastModel(getWeather())
+	saveData(data, context)
+	return data
+}
+
+private fun saveData(data: ArrayList<ForecastModel>, context: Context) {
+	val gsonBuilder = GsonBuilder()
+	val gson = gsonBuilder.create()
+	val json = gson.toJson(data)
+	val path = context.filesDir
+	val letDirectory = File(path, "json")
+	letDirectory.mkdirs()
+	val file = File(letDirectory, "data.json")
+	file.writeText(json)
+}
